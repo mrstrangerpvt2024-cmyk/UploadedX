1ff3ef7ed881eb38d31a8add6dfee54f720ec4f1
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2026-01-28 13:10:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -755,30 +755,30 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:
-                cc = f'╭━━━━━━━━━━━╮\n**🎥 VIDEO ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n' \
-        f'**📄 Title : {name1}** ᴍʀꜱᴛʀᴀɴɢᴇʀ™.mkv\n\n' \
-        f'**🔖 Batch :** `{b_name}`\n\n' \
+                cc = f'╭━━━━━━━━━━━╮\n**🎥 VIDEO ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n'
+        f'**📄 Title : {name1}** ᴍʀꜱᴛʀᴀɴɢᴇʀ™.mkv\n\n'
+        f'**🔖 Batch :** `{b_name}`\n\n'
         f'**📥 Extracted By : {CR}**\n'
-                cc1 = f'╭━━━━━━━━━━━╮\n**🎥 FILE ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n' \
-        f'**📄 Title : {name1}** ᴍʀꜱᴛʀᴀɴɢᴇʀ™.pdf\n\n' \
-        f'**🔖 Batch :** `{b_name}`\n\n' \
+                cc1 = f'╭━━━━━━━━━━━╮\n**🎥 FILE ID :** {str(count).zfill(3)}.\n╰━━━━━━━━━━━╯\n\n'
+        f'**📄 Title : {name1}** ᴍʀꜱᴛʀᴀɴɢᴇʀ™.pdf\n\n'
+        f'**🔖 Batch :** `{b_name}`\n\n'
         f'**📥 Extracted By : {CR}**\n'
-                cczip = f'[——— ✦ {str(count).zfill(3)} ✦ ———]({link0})\n\n' \
-        f'**🆔 𝑍𝐼𝑃 𝑰𝑑 :** `{count}`\n' \
-        f'**📁 𝑇𝑖𝑡𝑙𝑒 :** `{name1}`\n' \
-        f'**├── 𝑅𝑒𝑠𝑜𝑙𝑢𝑡𝑖𝑜𝑛 :** [{res}]\n\n' \
-        f'**📚 𝐶𝑜𝑢𝑟𝑠𝑒 :** {b_name}\n\n' \
+                cczip = f'[——— ✦ {str(count).zfill(3)} ✦ ———]({link0})\n\n'
+        f'**🆔 𝑍𝐼𝑃 𝑰𝑑 :** `{count}`\n'
+        f'**📁 𝑇𝑖𝑡𝑙𝑒 :** `{name1}`\n'
+        f'**├── 𝑅𝑒𝑠𝑜𝑙𝑢𝑡𝑖𝑜𝑛 :** [{res}]\n\n'
+        f'**📚 𝐶𝑜𝑢𝑟𝑠𝑒 :** {b_name}\n\n'
         f'**🌟 𝐸𝑥𝑡𝑟𝑎𝑐𝑡𝑒𝑑 𝐵𝑦 :** {CR}'
-                ccimg = f'**🏷️ Fɪʟᴇ ID :**  {str(count).zfill(3)}\n\n' \
-        f'**🖼️ Tɪᴛʟᴇ :** `{name1}`\n\n' \
-        f'<pre>📚 𝗕ᴀᴛᴄʜ : {b_name}</pre>\n\n' \
+                ccimg = f'**🏷️ Fɪʟᴇ ID :**  {str(count).zfill(3)}\n\n'
+        f'**🖼️ Tɪᴛʟᴇ :** `{name1}`\n\n'
+        f'<pre>📚 𝗕ᴀᴛᴄʜ : {b_name}</pre>\n\n'
         f'**🎓 Exᴛʀᴀᴄᴛ Bʏ : {CR}**\n'
                 ccm = f'**🏷️ Fɪʟᴇ ID :**  {str(count).zfill(3)}\n\n**🎞️ Tɪᴛʟᴇ :** `{name1} [{res}p] .mkv`\n\n<blockquote><b>📚 𝗕ᴀᴛᴄʜ :</b> {b_name}</blockquote>\n\n**🎓 Exᴛʀᴀᴄᴛ Bʏ : {CR}**\n'
-                cchtml = f'[——— ✦ {str(count).zfill(3)} ✦ ———]({link0})\n\n' \
-         f'**🆔 𝐻𝑇𝑀𝐿 𝑰𝑑 :** `{count}`\n' \
-         f'**🌐 𝑇𝑖𝑡𝑙𝑒 :** `{name1}`\n' \
-         f'**├── 𝑅𝑒𝑠𝑜𝑙𝑢𝑡𝑖𝑜𝑛 :** [{res}]\n\n' \
-         f'**📚 𝐶𝑜𝑢𝑟𝑠𝑒 :** {b_name}\n\n' \
+                cchtml = f'[——— ✦ {str(count).zfill(3)} ✦ ———]({link0})\n\n'
+         f'**🆔 𝐻𝑇𝑀𝐿 𝑰𝑑 :** `{count}`\n'
+         f'**🌐 𝑇𝑖𝑡𝑙𝑒 :** `{name1}`\n'
+         f'**├── 𝑅𝑒𝑠𝑜𝑙𝑢𝑡𝑖𝑜𝑛 :** [{res}]\n\n'
+         f'**📚 𝐶𝑜𝑢𝑟𝑠𝑒 :** {b_name}\n\n'
          f'**🌟 𝐸𝑥𝑡𝑟𝑎𝑐𝑡𝑒𝑑 𝐵𝑦 :** {CR}'
 
                 if "drive" in url:
